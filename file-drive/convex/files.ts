737352e76c7620ac86aca5b299b85e86ea426f7f
Steezy <bermudezalejandro45@gmail.com>
2024-04-22 17:54:53 -0400

    Added Search bar and functionality

--- file-drive/convex/files.ts
+++ file-drive/convex/files.ts
@@ -79,6 +79,7 @@ export const createFile = mutation({
 export const getFiles = query({
   args: {
     orgId: v.string(),
+    query: v.optional(v.string()),
   },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity();
@@ -98,10 +99,18 @@ export const getFiles = query({
       return [];
     }
     // return entries stored in this table
-    return ctx.db
+    const files = await ctx.db
       .query('files_table')
       .withIndex('by_orgId', (q) => q.eq('orgId', args.orgId))
       .collect();
+
+    const query = args.query;
+
+    if (query) {
+      return files.filter((file) => file.name.includes(query));
+    } else {
+      return files;
+    }
   },
 });
 
--- file-drive/src/app/page.tsx
+++ file-drive/src/app/page.tsx
@@ -8,10 +8,30 @@ import { UploadButton } from './upload-button';
 import { FileCard } from './file-card';
 import Image from 'next/image';
 import { Loader } from 'lucide-react';
+import { SearchBar } from './search-bar';
+import { useState } from 'react';
+
+function Placeholder() {
+  return (
+    <div className="flex flex-col gap-8 items-center mt-20">
+      <Image
+        alt="Image of a physical file upload."
+        width={300}
+        height={300}
+        src="/empty.svg"
+      />
+      <h2 className="text-2xl font-semibold text-purple-700">
+        No files to display, begin uploading!
+      </h2>
+      <UploadButton />
+    </div>
+  );
+}
 
 export default function Home() {
   const organization = useOrganization();
   const user = useUser();
+  const [query, setQuery] = useState('');
 
   let orgId: string | undefined = undefined;
   if (organization.isLoaded && user.isLoaded) {
@@ -19,7 +39,7 @@ export default function Home() {
   }
   // sends data from frontend to back end
   // data must be same as their first shaped
-  const files = useQuery(api.files.getFiles, orgId ? { orgId } : 'skip');
+  const files = useQuery(api.files.getFiles, orgId ? { orgId, query } : 'skip');
   const isLoading = files === undefined;
 
   return (
@@ -31,26 +51,15 @@ export default function Home() {
         </div>
       )}
 
-      {!isLoading && files.length === 0 && (
-        <div className="flex flex-col gap-8 items-center mt-20">
-          <Image
-            alt="Image of a physical file upload."
-            width={300}
-            height={300}
-            src="/empty.svg"
-          />
-          <h2 className="text-2xl font-semibold text-purple-700">
-            No files to display, begin uploading!
-          </h2>
-          <UploadButton />
-        </div>
-      )}
-      {!isLoading && files.length > 0 && (
+      {!isLoading && (
         <>
           <div className="flex justify-between items-center mb-7">
             <h1 className="text-4xl font-bold">Files</h1>
+            <SearchBar query={query} setQuery={setQuery} />
             <UploadButton />
           </div>
+
+          {files.length === 0 && <Placeholder />}
 
           <div className="grid grid-cols-3 gap-4">
             {files?.map((file) => {
